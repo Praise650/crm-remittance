e18ab416dedc4b2a7c24ffab50af27c287e92f9c
Praise650 <ayokanmi1275@gmail.com>
2025-12-04 13:02:14 +0100

    Added html page for health route

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // --- Import Routes ---
 const authRoutes = require("./routes/authRoutes");
@@ -22,10 +24,14 @@ const activityReportRoutes = require("./routes/activityReportRoutes");
 const analyticsRoutes = require("./routes/analyticsRoutes");
 
 // --- Health Check ---
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK", message: "Server running fine 🚀" });
-});
 
+// Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get("/api/health", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 // --- Mount Routes ---
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -51,14 +57,14 @@ app.use((err, req, res, next) => {
 mongoose.set("strictQuery", true);
 
 mongoose.connect(process.env.MONGODB_URI, { autoIndex: true })
-  .then(() => console.log("✅ MongoDB connected"))
+  .then(() => console.log("MongoDB connected"))
   .catch((err) => {
-    console.error("❌ MongoDB connection error:", err);
+    console.error("MongoDB connection error:", err);
     process.exit(1); // exit if cannot connect
   });
 
 // --- Start Server ---
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
